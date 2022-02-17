91b404d27ea0535306d042e4f5a4c53a9d19091b
unknown <prathmeshshinde105@gmail.com>
2022-02-17 13:42:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,316 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="banner">
+          <h1>Universal Adventures</h1>
+        </div>
+      </header>
+
+      <div className="section">
+        <div className="section-main">
+          <div className="section-sky">
+            <div className="skydive">
+              <p className="sky-text">Sky Diving</p>
+            </div>
+          </div>
+
+          <div className="section-scuba-bungee">
+            <div className="scuba">
+              <div className="scuba-img">
+                <img alt="scuba" src="Scuba.jpg" />
+              </div>
+              <div className="section-explore">
+                <p className="title">Scuba Diving in Grande Island, Goa</p>
+
+                <div className="hour">
+                  <div className="goa">
+                    <div>
+                      <img
+                        alt="clock"
+                        src="clock.png"
+                        style={{
+                          height: "20px",
+                          width: "20px",
+                        }}
+                      />
+                    </div>
+                    <div>
+                      <p>8 hours</p>
+                    </div>
+                  </div>
+
+                  <div className="goa">
+                    <div>
+                      <img
+                        alt="map"
+                        src="map.png"
+                        style={{
+                          height: "20px",
+                          width: "20px",
+                        }}
+                      />
+                    </div>
+                    <div>
+                      <p>Goa, India</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="activity">
+                  <div className="dos">
+                    <img
+                      alt="snacks"
+                      src="https://img.icons8.com/ios-filled/192/000000/knife-and-spatchula.png"
+                      style={{
+                        height: "45px",
+                        width: "auto",
+                      }}
+                    />
+                    <p>Snacks</p>
+                  </div>
+                  <div className="dos">
+                    <img
+                      alt="car"
+                      src="https://img.icons8.com/material/192/000000/car--v1.png"
+                      style={{
+                        height: "45px",
+                        width: "auto",
+                      }}
+                    />
+                    <p>Transport</p>
+                  </div>
+                  <div className="dos">
+                    <img
+                      alt="camera"
+                      src="https://img.icons8.com/material-rounded/192/000000/camera--v1.png"
+                      style={{
+                        height: "45px",
+                        width: "auto",
+                      }}
+                    />
+                    <p>Photos</p>
+                  </div>
+                  <div className="dos">
+                    <img
+                      alt="mountain"
+                      src="https://img.icons8.com/ios-filled/192/000000/mountain.png"
+                      style={{
+                        height: "45px",
+                        width: "auto",
+                      }}
+                    />
+                    <p>Activities</p>
+                  </div>
+                </div>
+                <div className="option">
+                  <p>Long tail boat ride</p>
+                  <p>Underwater Photos and Videos</p>
+                </div>
+              </div>
+              <div className="details">
+                <div className="from">
+                  <div>
+                    <div className="star">
+                      <div className="stars">
+                        <img
+                          alt="star"
+                          src="https://img.icons8.com/fluency/192/000000/star.png"
+                          style={{
+                            height: "20px",
+                            width: "20px",
+                          }}
+                        />
+                        <img
+                          alt="star"
+                          src="https://img.icons8.com/fluency/192/000000/star.png"
+                          style={{
+                            height: "20px",
+                            width: "20px",
+                          }}
+                        />
+                        <img
+                          alt="star"
+                          src="https://img.icons8.com/fluency/192/000000/star.png"
+                          style={{
+                            height: "20px",
+                            width: "20px",
+                          }}
+                        />
+                        <img
+                          alt="star"
+                          src="https://img.icons8.com/fluency/192/000000/star.png"
+                          style={{
+                            height: "20px",
+                            width: "20px",
+                          }}
+                        />
+                      </div>
+                      <div className="rating">
+                        <p>4.7</p>
+                      </div>
+                    </div>
+                    <div className="review">
+                      <p>36 reviews</p>
+                    </div>
+                  </div>
+                  <div className="end">
+                    <div className="start">
+                      <p>Starting from</p>
+                    </div>
+                    <div className="price">
+                      <p>₹ 1799</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="sec-view">
+                  <div className="view">
+                    <p>View Details</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bungee">
+              <div className="bungee-img">
+                <img alt="bungee" src="Bungee.jpg" />
+              </div>
+              <div className="section-explore">
+                <p className="title"> Bungee Jumping in Goa</p>
+
+                <div className="hour">
+                  <div className="goa">
+                    <div>
+                      <img
+                        alt="clock"
+                        src="clock.png"
+                        style={{
+                          height: "20px",
+                          width: "20px",
+                        }}
+                      />
+                    </div>
+                    <div>
+                      <p>30 min</p>
+                    </div>
+                  </div>
+
+                  <div className="goa">
+                    <div>
+                      <img
+                        alt="map"
+                        src="map.png"
+                        style={{
+                          height: "20px",
+                          width: "20px",
+                        }}
+                      />
+                    </div>
+                    <div>
+                      <p>Goa, India</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="activity">
+                  <div className="dos">
+                    <img
+                      alt="camera"
+                      src="https://img.icons8.com/material-rounded/192/000000/camera--v1.png"
+                      style={{
+                        height: "45px",
+                        width: "45px",
+                      }}
+                    />
+                    <p>Photos</p>
+                  </div>
+                  <div className="dos">
+                    <img
+                      alt="mountain"
+                      src="https://img.icons8.com/ios-filled/192/000000/mountain.png"
+                      style={{
+                        height: "45px",
+                        width: "45px",
+                      }}
+                    />
+                    <p>Activities</p>
+                  </div>
+                </div>
+                <div className="option">
+                  <p>61 meter height</p>
+                  <p>Jump Photo and Video</p>
+                  <p>Certificate</p>
+                </div>
+              </div>
+              <div className="details">
+                <div className="from">
+                  <div>
+                    <div className="star">
+                      <div className="stars">
+                        <img
+                          alt="star"
+                          src="https://img.icons8.com/fluency/192/000000/star.png"
+                          style={{
+                            height: "20px",
+                            width: "20px",
+                          }}
+                        />
+                        <img
+                          alt="star"
+                          src="https://img.icons8.com/fluency/192/000000/star.png"
+                          style={{
+                            height: "20px",
+                            width: "20px",
+                          }}
+                        />
+                        <img
+                          alt="star"
+                          src="https://img.icons8.com/fluency/192/000000/star.png"
+                          style={{
+                            height: "20px",
+                            width: "20px",
+                          }}
+                        />
+                        <img
+                          alt="star"
+                          src="https://img.icons8.com/fluency/192/000000/star.png"
+                          style={{
+                            height: "20px",
+                            width: "20px",
+                          }}
+                        />
+                      </div>
+                      <div className="rating">
+                        <p>4.7</p>
+                      </div>
+                    </div>
+                    <div className="review">
+                      <p>36 reviews</p>
+                    </div>
+                  </div>
+                  <div className="end">
+                    <div className="start">
+                      <p>Starting from</p>
+                    </div>
+                    <div className="price">
+                      <p>₹ 2800</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="sec-view">
+                  <div className="view">
+                    <p>View Details</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
